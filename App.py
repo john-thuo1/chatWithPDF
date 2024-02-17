68538d3638398b0f2541d142f54dde25820b6f6b
johnthuo <j.mwangi@alustudent.com>
2024-02-18 00:09:05 +0200

    Upgrade Dependencies and model to gpt3.5-turbo

--- App.py
+++ App.py
@@ -3,11 +3,11 @@ import os
 from PyPDF2 import PdfReader
 import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain import FAISS
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain_community.llms import OpenAI
+from langchain_community.callbacks import get_openai_callback
 
 # Load environment variables
 load_dotenv()
