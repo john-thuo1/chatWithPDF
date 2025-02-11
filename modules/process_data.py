6876e45d19864506469cacf57655c85b8ced7073
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2025-02-12 00:14:45 +0300

    update module logs

--- modules/process_data.py
+++ modules/process_data.py
@@ -9,14 +9,6 @@ Logger = setup_logger(logger_file="app")
 
 
 def extract_text_with_page_numbers(pdf) -> Tuple[str, List[int]]:
-    """Extract text from PDF and associate each text chunk with its page number.
-
-    Args:
-        pdf (PdfReader): The uploaded PDF document.
-
-    Returns:
-        Tuple[str, List[int]]: Extracted text and a list of corresponding page numbers.
-    """
     text = ""
     page_numbers = []
 
@@ -32,16 +24,6 @@ def extract_text_with_page_numbers(pdf) -> Tuple[str, List[int]]:
 
 
 def process_text_with_splitter(text: str, page_numbers: List[int]) -> FAISS:
-    """Process text with RecursiveCharacterTextSplitter and create a knowledge base using FAISS.
-
-    Args:
-        text (str): The extracted text from the PDF.
-        page_numbers (List[int]): List of corresponding page numbers for each text chunk.
-
-    Returns:
-        FAISS: A knowledge base created from the text chunks.
-    """
-    Logger.info("Starting text processing with RecursiveCharacterTextSplitter.")
 
     text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n\n", "\n", ".", " ", ""],
